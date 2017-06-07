1f9251e7d32394f0de4f3014b19ced19fb7a8fc0
tkeesling <tyler.keesling@gmail.com>
2017-06-07 15:03:13 -0600

    refactor and comment

--- palindrome/palindrome.js
+++ palindrome/palindrome.js
@@ -1,10 +1,14 @@
-var myString = "No 'x'* in_Nixon?"
+var myString = "No 'x'* ins_Nixon?"
 
 function palindrome(string) {
-  let lowerCase = string.toLowerCase()
-  let newString = lowerCase.replace(/[^a-z]/g, '')
-  // console.log(newString)
-  return newString === newString.split('').reverse().join('')
+	// set to lowercase
+	// remove special characters and spacing
+	// set lowercased, special char removed string to reversed string
+	// reverse string by splitting to an array, reversing array, then joining back to string
+	let lowerCase = string.toLowerCase()
+	let newString = lowerCase.replace(/[^a-z]/g, '')
+	console.log(newString)
+	return newString === newString.split('').reverse().join('')
 }
 
 palindrome(myString)
